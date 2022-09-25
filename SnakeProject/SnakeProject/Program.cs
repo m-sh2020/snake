b074e0cfec48ae1eaa3ff9cae9b3c79b5f38f34d
msh <m.sh1900@yandex.ru>
2022-09-25 23:29:11 +0300

    Hello word

---
+++ SnakeProject/SnakeProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
