11be1336ffa492c095436409d57072df495336a7
msh <m.sh1900@yandex.ru>
2022-09-26 12:35:02 +0300

    Класс Point

---
+++ SnakeProject/SnakeProject/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeProject
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
+        }
+    }
+}
