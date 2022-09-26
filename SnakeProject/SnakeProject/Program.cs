c7e925e36118b80029bac82f37c4853702cb8c10
msh <m.sh1900@yandex.ru>
2022-09-26 23:27:58 +0300

    Изменения с конструктором

--- SnakeProject/SnakeProject/Program.cs
+++ SnakeProject/SnakeProject/Program.cs
@@ -6,8 +6,14 @@ namespace SnakeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(1,3,'*');
+            p1.Draw();
+
+            Point p2 = new Point(4,5,'#');
+            p2.Draw();
+
             Console.ReadLine();
+
         }
     }
 }
