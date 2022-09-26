911d4bd4f1b8345e1624e6ffe367e9e38aceb077
msh <m.sh1900@yandex.ru>
2022-09-27 01:12:35 +0300

    Добавлен список

--- SnakeProject/SnakeProject/Program.cs
+++ SnakeProject/SnakeProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakeProject
 {
@@ -6,12 +7,31 @@ namespace SnakeProject
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
+             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
             Console.ReadLine();
 
         }
