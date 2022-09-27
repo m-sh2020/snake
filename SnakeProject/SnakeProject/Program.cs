24f3300712f741f715265dc609688f2116b943d8
msh <m.sh1900@yandex.ru>
2022-09-27 23:18:19 +0300

    Добавлены классы с вертикальными и горизонтальными линиями

--- SnakeProject/SnakeProject/Program.cs
+++ SnakeProject/SnakeProject/Program.cs
@@ -7,31 +7,17 @@ namespace SnakeProject
     {
         static void Main(string[] args)
         {
-             Point p1 = new Point(1,3,'*');
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorisontalLine line = new HorisontalLine(5,10,8,'+');
+            line.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine vline = new VerticalLine(5, 10, 15, '/');
+            vline.Draw();
             Console.ReadLine();
 
         }
---
+++ SnakeProject/SnakeProject/VerticalLine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeProject
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yTop, int yButton, int x, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int y = yTop; y <= yButton; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+
+            //Point p1 = new Point(4, 4, '*');
+            //Point p2 = new Point(5, 4, '*');
+            //Point p3 = new Point(6, 4, '*');
+            //pList.Add(p1);
+            //pList.Add(p2);
+            //pList.Add(p3);
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
