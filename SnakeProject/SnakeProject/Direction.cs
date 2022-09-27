702c5d9b95f69d58f08ad4d3c0917970ca1c1c24
msh <m.sh1900@yandex.ru>
2022-09-28 00:41:30 +0300

    Создание класса змейки

---
+++ SnakeProject/SnakeProject/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeProject
+{
+    //направление
+        enum Direction 
+        {
+            LEFT,
+            RIGHT,
+            UP,
+            DOWN
+        }
+}
--- SnakeProject/SnakeProject/Program.cs
+++ SnakeProject/SnakeProject/Program.cs
@@ -23,7 +23,9 @@ namespace SnakeProject
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p,4,Direction.RIGHT);
+            snake.Draw();
+            Console.ReadLine();
         }
     }
 }
